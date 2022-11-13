7449fcf2604b11d7b3bb86698f173b1104c6618e
Akash <akashdas0599@example.com>
2022-11-13 15:36:09 +0530

    Improved Styling

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,16 @@
 import { Box, Stack } from "@mui/material";
-import React, { isValidElement } from "react";
+import React from "react";
 import { VideoCard, ChannelCard } from "./";
 
-const Videos = ({ videos }) => {
-  console.log(videos);
+const Videos = ({ videos, direction }) => {
+  // if (!videos?.lenght) return "Loading....";
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
