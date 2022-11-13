e04ebc0d59f1d537daef12f9661e9fab7da7f61c
Akash <akashdas0599@example.com>
2022-11-13 15:26:16 +0530

    Added Videos to Channel Detail

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Video, ChannelCard } from "./";
+import { Videos, ChannelCard } from "./";
 
 const ChannelDetail = () => {
   const [channelDetail, setChannelDetail] = useState(null);
@@ -15,22 +15,31 @@ const ChannelDetail = () => {
     fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
       setChannelDetail(data?.items[0])
     );
-    fetchFromAPI(`search?channelId=${id}&part=snippet&order=data`).then(
-      (data) => setVideos(data?.items[0])
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
     );
   }, [id]);
 
   return (
     <Box minHeight="95vh">
       <Box>
-        <div style={{ background: "white", height: " 300px" }} />
+        <div
+          style={{
+            background:
+              "linear-gradient(90deg, rgba(58,55,113,1) 0%, rgba(165,117,201,1) 39%, rgba(134,135,211,1) 72%, rgba(98,228,255,1) 100%)",
+            height: " 300px",
+          }}
+        />
         {channelDetail && (
           <ChannelCard channelDetail={channelDetail} marginTop="-110px" />
         )}
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: "100px" } }} />
+        {videos && <Videos videos={videos} />}
       </Box>
     </Box>
   );
 };
 
 export default ChannelDetail;
-https://youtu.be/FHTbsZEJspU?t=6342
