17ad7e0937eacaf1f3e4e790582403626e55c881
Akash Das <akashdas0599@gmail.com>
2023-03-08 18:50:06 +0530

    fix: Search Feed not updating

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -21,6 +21,12 @@ const SearchFeed = () => {
     }
   }, [loading]);
 
+  useEffect(() => {
+    setVideos(null);
+    setnextPageToken("");
+    setLoading(true);
+  }, [searchTerm]);
+
   return (
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
       <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
